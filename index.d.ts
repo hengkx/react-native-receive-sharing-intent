a4f190b8e762fa3868c31ae6b03c55db514e3695
hengkx <ycxzhkx@gmail.com>
2020-07-26 20:55:02 +0800

    fix ios chinese and add ts

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+type Handler = (files: any[]) => void;
+type ErrorHandler = (error: any) => void;
+
+declare class ReceiveSharingIntentModule {
+  static getReceivedFiles: (handler: Handler, error: ErrorHandler) => void;
+  static getFileNames: (
+    handler: Handler,
+    error: ErrorHandler,
+    url?: string,
+  ) => void;
+  static clearReceivedFiles: () => void;
+}
+
+export default ReceiveSharingIntent;
